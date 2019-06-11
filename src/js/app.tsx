fad2fd6261df642c8f86acb899f42c9ec77a4085
BenasdTW <ben6312tt@gmail.com>
2019-06-12 01:17:05 +0800

    sample code for react

--- src/js/app.tsx
+++ src/js/app.tsx
@@ -3,8 +3,7 @@ import * as ReactDOM from 'react-dom';
 
 // const scss1 = require("../css/style.scss");
 import { Test } from "./paragraph";
+import { Hello } from "./hello";
 import "../css/style.scss";
 
-let element = <h1>Hello everyone!</h1>
-
-ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<div><Test /><Hello compiler="webpack" framework="React" /></div>, document.getElementById('root'));
